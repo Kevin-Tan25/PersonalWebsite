17bee1eb105b13697cf4afff33efbb2977bff1c4
Kevin Tan <kevintan@Kevins-MacBook-Pro-2.local>
2022-05-17 11:09:50 -0700

    started working on about component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
 import './about.css';
+import ME from '../../assets/me-about.jpg';
+import { FaAward } from 'react-icons/fa';
 
 const About = () => {
-  return <section id='about'>about</section>;
+  return (
+    <section id='about'>
+      <h5>Get to Know</h5>
+      <h1>About Me</h1>
+
+      <div className='container about__container'>
+        <div className='about__me'>
+          <div className='about__me-image'>
+            <img src={ME} alt='About Image' />
+          </div>
+        </div>
+
+        <div className='about__content'>
+          <div className='about__cards'>
+            <article className='about__card'>
+              <FaAward className='about__icon' />
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
