6440a41abc51681524ddf87cc68811dc439498ce
Kevin Tan <kevintan@dhcp-128-189-211-239.ubcsecure.wireless.ubc.ca>
2022-05-18 14:58:05 -0700

    finished about component. stylized containers

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './about.css';
 import ME from '../../assets/me-about.jpg';
 import { FaAward } from 'react-icons/fa';
+import { FiUsers } from 'react-icons/fi';
+import { VscFolderLibrary } from 'react-icons/vsc';
 
 const About = () => {
   return (
@@ -23,7 +25,27 @@ const About = () => {
               <h5>Experience</h5>
               <small>3+ Years Working</small>
             </article>
+
+            <article className='about__card'>
+              <FiUsers className='about__icon' />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon' />
+              <h5>Projects</h5>
+              <small>80+ Completed</small>
+            </article>
           </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat
+            modi qui sit enim quod cupiditate obcaecati. Nihil fugit eius odio
+            modi atque corrupti, ab impedit soluta iusto iste! A, delectus.
+          </p>
+          <a href='#contact' className='btn btn-primary'>
+            Let's Talk
+          </a>
         </div>
       </div>
     </section>
